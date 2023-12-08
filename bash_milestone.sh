c12a886798f2dd85af6304ae55c030f3201f9045
choncey <nchoncey@gmail.com>
2023-12-08 12:15:50 -0800

    edited script

--- bash_milestone.sh
+++ bash_milestone.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #Ask the user a question
 
-        echo Do you want to go to jail, or do you want to go home?
         echo Do you want the hook up?
 
-if [ condition = 'home' ]
 if [ condition = 'yes' ]
 then
-        echo Glad you want to go home.
         echo Aight, I got you.
 
 # this will install apache2
